c04bf229238bda2e09aa5233af707764168dd4ad
rodrigolima-dev <rodrigolima.developer@gmail.com>
2024-07-09 14:31:25 -0300

    Configurando Stack

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
+import {Container, ButtonPost} from './styles'
+
+import Feather from 'react-native-vector-icons/Feather'
+
 
 export default function Home() {
- return (
-   <View/>
+  return(
+    <Container>
+      <ButtonPost onPress={() => Alert.alert('write a post.')}>
+        <Feather name="edit-2" color="#fff" size={25}/>
+      </ButtonPost>
+    </Container>
   );
+ 
 }
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,13 +2,27 @@ import React from "react";
 import Feather from 'react-native-vector-icons/Feather'
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from 'react-native';
 
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import Profile from "../pages/Profile";
+import NewPost from "../pages/NewPost";
+import PostsUser from "../pages/PostsUser";
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackScreen() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+            <Stack.Screen name="NewPost" component={NewPost} />
+            <Stack.Screen name="PostsUser" component={PostsUser} />
+        </Stack.Navigator>
+    );
+}
 
 function AppRoutes() {
     return(
@@ -20,6 +34,7 @@ function AppRoutes() {
                 backgroundColor: '#202225',
                 borderTopWidth: 0
             },
+            headerShown: false,
 
             tabBarActiveTintColor: '#fff'
         }}
@@ -27,7 +42,7 @@ function AppRoutes() {
         >
             <Tab.Screen 
             name="Home" 
-            component={Home}
+            component={StackScreen}
             options={{
                 tabBarIcon: ({color, size}) => {
                     return <Feather name="home" color={color} size={size}/>
