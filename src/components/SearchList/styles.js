b24f58bba2f1781681b9e35df09779e55bc7f70b
rodrigolima-dev <rodrigolima.developer@gmail.com>
2024-07-17 16:55:08 -0300

    Página de search.

---
+++ src/components/SearchList/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+    margin: 5px 10px;
+    background-color: #202225;
+    border-color: gray;
+    border-width: 0.3px;
+    padding: 15px;
+    border-radius: 7px;
+`;
+
+
+export const Name = styled.Text`
+    color: #fff;
+    font-size: 16px;
+` 
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,7 +1,67 @@
 import { View } from 'react-native';
+import { Container, AreaInput, Input, List } from './styles'
+import { useEffect, useState } from 'react';
+
+import Feather from 'react-native-vector-icons/Feather'
+import firestore from '@react-native-firebase/firestore'
+import SearchList from '../../components/SearchList'
 
 export default function Search() {
- return (
-   <View/>
+  const [input, setInput] = useState('');
+  const [users, setUsers] = useState([]);
+
+
+  useEffect(() => {
+    if(input === '' || input === undefined) {
+      setUsers([])
+      return;
+    }
+
+    const subscriber = firestore().collection('users')
+    .where('name', '>=', input)
+    .where('name', '<=', input + '\uf8ff')
+    .onSnapshot( snapshot => {
+      const usersList = [];
+
+      snapshot.forEach(doc => {
+        usersList.push({
+          ...doc.data(),
+          id: doc.id
+        });
+      });
+
+      setUsers(usersList)
+      console.log(usersList)
+
+    });
+
+    return () => subscriber();
+
+  },[input])
+
+  return (
+    <Container>
+      <AreaInput>
+        <Feather
+        name='search'
+        color='#28A745'
+        size={20}
+        />
+
+        <Input
+        placeholder='Looking for someone?'
+        placeholderTextColor={'#202225'}
+        value={input}
+        onChangeText={(text) => setInput(text)}
+        />
+      </AreaInput>
+
+      <List
+        showsVerticalScrollIndicator={false}
+        data={users}
+        keyExtractor={(item) => item.id }
+        renderItem={({item}) => <SearchList data={item}/>}
+      />
+    </Container>
   );
 }
